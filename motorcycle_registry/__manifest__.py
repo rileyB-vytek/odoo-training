e2cdff5e4637529c7f59ddff27dea47895203bac
rileyB-vytek <prbrannon@vytek.com>
2023-08-07 18:56:18 +0000

    [ADD] New Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': """Manage Registration of Motorcycles""",
+    'description': """Motorcycle Registry
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership
+    of each motorcycled of the brand.""",
+    'author': 'rileyB-vytek',
+    'version': '0.1',
+    'category': 'Kawaiil/Custom Modules',
+    'website': 'https://github.com/rileyB-vytek/odoo-training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
