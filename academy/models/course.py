90daca9b95a3a423bf324125e8cc1268d78e93aa
rileyB-vytek <prbrannon@vytek.com>
2023-08-08 18:00:49 +0000

    [ADD] New Course Model and Fields

--- academy/models/course.py
+++ academy/models/course.py
@@ -1,6 +1,17 @@
-from odoo import models
+from odoo import fields, models
 
 class Course(models.Model):
     _name = "academy.course"
     _description = "Course Info"
-    
+
+    name = fields.Char(string="Title", required=True)
+    active = fields.Boolean(string="Active", default=True)
+
+    description = fields.Text()
+    level = fields.Selection(string="Level",
+                             selection=[
+                                 ('beginner','Beginner'),
+                                 ('intermediate','Intermediate'),
+                                 ('advanced','Advanced'),
+                             ],
+                             copy=False)
