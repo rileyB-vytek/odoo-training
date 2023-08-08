b4851fa959a7ab86f5b20ef40af0744d2150659a
rileyB-vytek <prbrannon@vytek.com>
2023-08-08 13:07:46 +0000

    [ADD] New Academy Module

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Module to handle Course and Sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'rileyB-vytek',
+    'website': 'https://github.com/rileyB-vytek/odoo-training',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
