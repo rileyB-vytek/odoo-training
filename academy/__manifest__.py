5cad6bf23be1719f22c4f6c91993cf8a81a5f135
rileyB-vytek <prbrannon@vytek.com>
2023-08-09 13:24:53 +0000

    [ADD] Security and Menu Items

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -11,8 +11,15 @@
     'website': 'https://github.com/rileyB-vytek/odoo-training',
     'category': 'Custom Modules/Tech Training',
     'depends': ['base'],
-    'data': [],
-    'demo': [],
+    'data': [
+        'security/academy_groups.xml',
+        'security/ir.model.access.csv',
+        'security/academy_security.xml',
+        'views/academy_menuitems.xml',
+    ],
+    'demo': [
+        'demo/course_demo.xml',
+    ],
     'application': True,
     'version': '0.2',
 }
