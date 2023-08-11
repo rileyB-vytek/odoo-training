4c6d9d37ace98ebdf06ee13af20988a720fa89b3
rileyB-vytek <prbrannon@vytek.com>
2023-08-11 13:31:29 +0000

    [ADD] Advanced Fields Practice

--- academy/models/course.py
+++ academy/models/course.py
@@ -1,19 +1,34 @@
-from odoo import fields, models
-
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 class Course(models.Model):
     _name = "academy.course"
     _description = "Course Info"
 
+    #Reserved Fields
     name = fields.Char(string="Title", required=True)
     active = fields.Boolean(string="Active", default=True)
 
+    # Simple Fields
     image = fields.Image(string="Image")
-    
     description = fields.Text()
     level = fields.Selection(string="Level",
-                             selection=[
-                                 ('beginner','Beginner'),
+                             selection=[('beginner','Beginner'),
                                  ('intermediate','Intermediate'),
-                                 ('advanced','Advanced'),
-                             ],
+                                 ('advanced','Advanced'),],
                              copy=False)
+
+    session_ids = fields.One2many(comodel_name="academy.session", string="Session", inverse_name="course_id")
+
+    currency_id = fields.Many2one(comodel_name="res.currency", string="Currency", default= lambda self:self.env.company.currency_id.id)
+
+    base_price = fields.Monetary(string="Base Price", currency_field="currency_id")
+    additional_fee = fields.Monetary(string="Additional Fee", currency_field="currency_id")
+    total_price = fields.Monetary(string="Total Price", currency_field="currency_id", compute="_compute_total_price")
+
+    @api.depends("base_price", "additional_fee")
+    def _compute_total_price(self):
+        for record in self:
+            if(record.base_price < 0):
+                raise ValidationError(('Base Price can not be negative'))
+            record.total_price = record.base_price + record.additional_fee
+        
--- academy/models/session.py
+++ academy/models/session.py
@@ -1,18 +1,30 @@
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.tools import date_utils
 
 
 class Session(models.Model):
     _name = 'academy.session'
     _description = 'Session Info'
 
-    name = fields.Char(string="Title")
+    name = fields.Char(string="Title", related="course_id.name", readonly=False)
+    
     session_number = fields.Char(string="Session Number", default="S0000", copy=False, required=True, readonly=True)
     
     date_start = fields.Datetime(string="Start Date", required=True)
     
     date_end = fields.Datetime(string="End Date", required=True)
 
+    duration = fields.Integer(string="Session Duration", compute="_compute_session_duration", inverse="_inverse_session_duration", readonly=False)
+    
+    course_id = fields.Many2one(comodel_name="academy.course", string="Course", ondelete="cascade", required=True)
+
+    instructor_id = fields.Many2one(comodel_name="res.users", string="Instructor", ondelete="restrict")
+
+    student_ids = fields.Many2many(comodel_name="res.partner", string="Students")
+
+    description = fields.Text(related="course_id.description")
+    
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
@@ -25,3 +37,15 @@ class Session(models.Model):
         for session in self:
             if(session.date_start > session.date_end):
                 raise ValidationError('The end date can not be before the start date!')
+
+    @api.depends("date_start", "date_end")
+    def _compute_session_duration(self):
+        for record in self:
+            if record.date_start and record.date_end:
+                record.duration = (record.date_end - record.date_start).days + 1
+        
+    def _inverse_session_duration(self):
+        for record in self:
+            if record.date_start and record.duration:
+                record.date_end = date_utils.add(record.date_start, days=record.duration-1)
+                
