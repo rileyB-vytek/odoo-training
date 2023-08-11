8df4f915ebb6d800136c96ce0c6b61eb0bcb7afe
rileyB-vytek <prbrannon@vytek.com>
2023-08-11 20:14:01 +0000

    [ADD] Task 9 and 10 Exercise

--- motorcycle_registry/models/registry.py
+++ motorcycle_registry/models/registry.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 import re
 
 class Registry(models.Model):
@@ -8,17 +9,19 @@ class Registry(models.Model):
 
     registry_number = fields.Char(string="Motorcycle Registry Number (MRN)", default="MRN0000", copy=False, required=True, readonly=True)
     
-    vin = fields.Char(string="VIN", compute="_compute_registry_vin", required=True, readonly=True)
-    make = fields.Char(string="Make", required=True)
-    model = fields.Char(string="Model", required=True)
-    year = fields.Char(string="Year", required=True)
-    battery_capacity = fields.Char(string="Battery Capacity", required=True)
-    serial_number = fields.Char(string="Serial Number", required=True)
+    vin = fields.Char(string="VIN", required=True)
+    make = fields.Char(string="Make", readonly=True, compute="_compute_make")
+    model = fields.Char(string="Model", readonly=True, compute="_compute_model")
+    year = fields.Char(string="Year", readonly=True, compute="_compute_year")
     
     license_plate = fields.Char(string="License Plate")
+
+    owner_id = fields.Many2one(comodel_name="res.partner", string="Owner")
+    #first_name = fields.Char(string="First", required=True)
+    #last_name = fields.Char(string="Last", required=True)
+    email = fields.Char(related="owner_id.email")
+    phone = fields.Char(related="owner_id.phone")
     
-    first_name = fields.Char(string="First", required=True)
-    last_name = fields.Char(string="Last", required=True)
     image = fields.Image(string="Image")
     current_mileage = fields.Float(string="Current Mileage")
     certificate_title = fields.Binary(string="Certificate Title")
@@ -30,10 +33,48 @@ class Registry(models.Model):
             if vals.get('registry_number', ('MRN0000')) == ('MRN0000'):
                 vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
         return super().create(vals_list)
-
-    @api.depends("make", "model", "year", "battery_capacity", "serial_number")
-    def _compute_session_duration(self):
+                
+    @api.constrains("vin")
+    def _check_vin_pattern(self):
+        for registry in self(lambda r: r.vin):
+            match = re.match("^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{5}$", registry.vin)
+            if not match:
+                raise ValidationError("""The VIN must match the specified pattern.
+                ● Make - 2 Capital Letters
+                ● Model - 2 Capital Letters
+                ● Year - 2 Digits
+                ● Battery Capacity - 2 Capital Letters or Numbers
+                ● Serial Number - 6 Digits""")
+                
+    @api.constrains("license_plate")
+    def _check_license_plate_pattern(self):
+        for registry in self.filtered(lambda r: r.license_plate):
+            match = re.match("^[A-Z]{1,3}\d{1,4}[A-Z]{0,2}$", registry.license_plate)
+            if not match:
+                raise ValidationError("""The License Plate must match the specified pattern.
+                ● 1 - 4 Capital Letters
+                ● 1 - 3 Digits
+                ● Optional 2 Capital Letters""")
+    
+    @api.depends("vin")
+    def _compute_make(self):
         for record in self:
-            if record.make and record.model and record.year and record.battery_capacity and record.serial_number:
-                record.vin = re.match("[A-Z]{2}", record.make) + re.match("[A-Z]{2}", record.model) + re.match("\d{2}", record.year) + re.match("[A-Z0-9]{2}", record.battery_capacity) + re.match("\d{5}", record.serial_number)
+            if record.vin:
+                # Use regex to grab make portion of VIN (Make = first 2 capital letters)
+                record.make = record.vin[:1] + record.vin[1:2]
+                
+    @api.depends("vin")
+    def _compute_model(self):
+        for record in self:
+            if record.vin:
+                # Use regex to grab model portion of VIN (Make = second 2 capital letters)
+                record.model = record.vin[2:3] + record.vin[3:4]
+                
+    @api.depends("vin")
+    def _compute_year(self):
+        for record in self:
+            if record.vin:
+                # Use regex to grab year portion of VIN (Make = first 2 digits)
+                record.year = record.vin[4:5] + record.vin[5:6]
+
                 
