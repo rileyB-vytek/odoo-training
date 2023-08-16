4a50cd078378ac5124cb1dd0179b74b202fbf780
rileyB-vytek <prbrannon@vytek.com>
2023-08-16 14:38:45 +0000

    [ADD] New Model for Website Compare Page

---
+++ motorcycle_compare/controllers/motorcycle_compare_controllers.py
@@ -0,0 +1,9 @@
+from odoo import http
+
+class Motorcycle_Compare(http.Controller):
+    @http.route('/motorcycle/compare/', auth='public', website=True)
+    def courses(self, **kw):
+        motorcycles = http.request.env['product.template'].search([('detailed_type', '=', 'motorcycle')])
+        return http.request.render('motorcycle_compare.motorcycle_compare_website', {
+            'motorcycles': motorcycles,
+        })
