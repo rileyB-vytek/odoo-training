79e1fbbb62c4b6e2e2b9ec9206540b4907598e5d
rileyB-vytek <prbrannon@vytek.com>
2023-08-17 12:07:47 +0000

    [FIX] Final Fixes and Edits

--- motorcycle_compare/controllers/motorcycle_compare_controllers.py
+++ motorcycle_compare/controllers/motorcycle_compare_controllers.py
@@ -2,7 +2,7 @@ from odoo import http
 
 class Motorcycle_Compare(http.Controller):
     @http.route('/motorcycle/compare/', auth='public', website=True)
-    def courses(self, **kw):
+    def motorcycle_compare(self, **kw):
         motorcycles = http.request.env['product.template'].search([('detailed_type', '=', 'motorcycle')])
         return http.request.render('motorcycle_compare.motorcycle_compare_website', {
             'motorcycles': motorcycles,
